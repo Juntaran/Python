bf7d89dd5a0d01ddddd88387c4fa35541a8dc7fc
Juntaran <jacinthmail@gmail.com>
2016-04-26 02:19:12 +0800

    2016.4.26

---
+++ Black Hat/Sniffer.py
@@ -0,0 +1,29 @@
+﻿import socket
+import os
+
+#监听的主机
+host = "192.168.1.104"
+
+#原始套接字
+if os.name == "nt":
+	socket_protocol = socket.IPPROTO_IP
+else:
+	socket_protocol = socket.IPPROTO_ICMP
+	
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host,0))
+
+#设置在捕获的数据包中包含IP头
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+#在Windows平台设置IOCTL启用混杂模式
+if os.name == "nt":
+	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+	
+#读取单个数据包
+print sniffer.recvfrom(65565)
+
+#在Windows平台关闭混杂模式
+if os.name == "nt":
+	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
---
+++ Black Hat/TcpClient.py
@@ -0,0 +1,18 @@
+﻿import socket
+
+target_host = "www.sina.com"
+target_port = 80
+
+#建立一个socket对象
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#连接客户端
+client.connect((target_host, target_port))
+
+#发送数据
+client.send("GET / HTTP/1.1\r\nHost: sina.com\r\n\r\n")
+
+#接收数据
+response = client.recv(4096)
+
+print response
